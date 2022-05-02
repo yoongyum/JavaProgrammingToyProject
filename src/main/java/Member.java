00682b3cc4d12eb6efb91d355f25e416a9a5fcdb
yoongyum <imshreg@naver.com>
2022-05-02 20:16:10 +0900

    feat: Create Member.java

---
+++ src/main/java/Member.java
@@ -0,0 +1,39 @@
+public class Member {
+
+    private String id;  //Primary Key
+    private String name;
+    private String phoneNumber;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
