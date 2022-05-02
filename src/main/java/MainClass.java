04d09ca7aa159521ce62f459ac83a7be7029a8a0
yoongyum <imshreg@naver.com>
2022-05-02 20:29:47 +0900

    feat: Build printCommandGuide() and creating readMenu
    1. 명령어 가이드라인 생성
    2. readMenu명령어 0번 구현

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
+
 public class MainClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("#############################");
         System.out.println("### 회원 관리 프로그램 START ##");
         System.out.println("#############################");
@@ -8,7 +10,7 @@ public class MainClass {
         manager.readMenu();
 
         System.out.println("#############################");
-        System.out.println("### GOOD-BYE 프로그램 종료 ###");
+        System.out.println("#### GOOD-BYE 프로그램 종료 ####");
         System.out.println("#############################");
     }
 }
--- src/main/java/MemberManager.java
+++ src/main/java/MemberManager.java
@@ -1,6 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MemberManager {
 
-    public void readMenu(){
+    public void readMenu() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            printCommandGuide();
+            var cmd = br.readLine();
+            switch (cmd){
+                case "0": //0일경우 readMenu() 종료..
+                    return;
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
 
+            }
+        }
+    }
+
+    public void printCommandGuide(){
+        System.out.println("목록을 원하시면 1번을 입력하세요.");
+        System.out.println("등록을 원하시면 2번을 입력하세요.");
+        System.out.println("수정을 원하시면 3번을 입력하세요.");
+        System.out.println("삭제를 원하시면 4번을 입력하세요.");
+        System.out.println("종료을 원하시면 0번을 입력하세요.");
     }
 }
