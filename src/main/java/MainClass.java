eafbed5322a239969a0db3d2e24700cd4cd7e0f8
yoongyum <imshreg@naver.com>
2022-05-02 20:16:22 +0900

    feat: Create MemberManager.java

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,14 @@
 public class MainClass {
     public static void main(String[] args) {
+        System.out.println("#############################");
+        System.out.println("### 회원 관리 프로그램 START ##");
+        System.out.println("#############################");
 
+        MemberManager manager = new MemberManager();
+        manager.readMenu();
+
+        System.out.println("#############################");
+        System.out.println("### GOOD-BYE 프로그램 종료 ###");
+        System.out.println("#############################");
     }
 }
---
+++ src/main/java/MemberManager.java
@@ -0,0 +1,6 @@
+public class MemberManager {
+
+    public void readMenu(){
+
+    }
+}
